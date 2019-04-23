6d2ec3ab87068f769a94aba4c2257bc9a963bd52
lucas <lucasjoao.lj@gmail.com>
2019-04-23 01:30:13 -0300

    Add doc in TokenType.java

--- src/main/java/br/ufsc/ine5426/compiladorxpp/lexicalanalyser/TokenType.java
+++ src/main/java/br/ufsc/ine5426/compiladorxpp/lexicalanalyser/TokenType.java
@@ -1,14 +1,49 @@
+/*
+ *  Trabalho realizado para a disciplina INE5426 (Construção de Compiladores) do
+ *  curso de Ciência da Computação da Universidade Federal de Santa Catarina.
+ *  Disciplina ministrada pelo prof. Alvaro Franco.
+ *
+ *  Desenvolvido por:
+ *  Christian de Pieri, Jacyara Bosse, Lucas João Martins e Nathália Liz de Brito.
+ *
+ */
+
 package br.ufsc.ine5426.compiladorxpp.lexicalanalyser;
 
+/**
+ * Define os tipos de tokens que a linguagem X++ possui.
+ *
+ */
 public enum TokenType {
+	/**
+	 * Palavra reservada.
+	 */
 	PR,
 	IDENT,
+	/**
+	 * Delimitador.
+	 */
 	DELIMITER,
+	/**
+	 * Operador relacional.
+	 */
 	RELOP,
+	/**
+	 * Operador aritmético.
+	 */
 	AROP,
+	/**
+	 * Abertura de bloc.
+	 */
 	BLOCK_OPEN,
+	/**
+	 * Fechamento de bloco.
+	 */
 	BLOCK_CLOSE,
 	INT_CONSTANT,
 	STRING_CONSTANT,
+	/**
+	 * Ponto (.).
+	 */
 	POINT;
 }
