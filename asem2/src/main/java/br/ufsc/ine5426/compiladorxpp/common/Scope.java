97290c3ed184596bb8a5d732b4faf7d81cb1e322
lucas <lucasjoao.lj@gmail.com>
2019-07-05 03:28:09 -0300

    :heart: lombok :heart:

--- asem2/src/main/java/br/ufsc/ine5426/compiladorxpp/common/Scope.java
+++ asem2/src/main/java/br/ufsc/ine5426/compiladorxpp/common/Scope.java
@@ -1,35 +1,16 @@
 package br.ufsc.ine5426.compiladorxpp.common;
 
-import java.util.Objects;
-
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Scope {
 
+	@EqualsAndHashCode.Include
 	private int id;
 	private ScopeType type;
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof Scope)) {
-			return false;
-		}
-		Scope other = (Scope) obj;
-		return this.id == other.id;
-	}
 
 }
