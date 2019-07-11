dfaa0e60dcbf1df112f75c4eba6e45c7d59091ed
lucas <lucasjoao.lj@gmail.com>
2019-07-11 00:58:44 -0300

    God bless save actions + icg to idents.

--- asem2/src/main/java/br/ufsc/ine5426/compiladorxpp/intermediatecodegenerator/IntermediateCodeGenerator.java
+++ asem2/src/main/java/br/ufsc/ine5426/compiladorxpp/intermediatecodegenerator/IntermediateCodeGenerator.java
@@ -10,115 +10,138 @@ import br.ufsc.ine5426.compiladorxpp.common.TokenType;
 import br.ufsc.ine5426.compiladorxpp.semanticanalyzer.SemanticAnalyzer;
 
 public class IntermediateCodeGenerator {
-	
-		private List<Token> tokens;
-		private StringBuilder intermediateCode;
-		private static final String BREAK_LINE = "\n";
-		private Set<Integer> seenIds;
-		
-		private int labelCounter;
-		// XXX: salvar ultima label de saída para break???
-	
-		public IntermediateCodeGenerator(SemanticAnalyzer semanticAnalyzer) {
-			this.tokens = semanticAnalyzer.getTokens();
-			this.intermediateCode = new StringBuilder();
-			this.seenIds = new HashSet<Integer>();
-			this.labelCounter = 0;
-		}
 
-		public void run() {
-			for (int i = 0; i < tokens.size(); i++) {
-				if (!seenIds.contains(i)) {
-					seenIds.clear();
-					operate(i);
-				}
+	private List<Token> tokens;
+	private StringBuilder intermediateCode;
+	private static final String BREAK_LINE = "\n";
+	private Set<Integer> seenIds;
+
+	private int labelCounter;
+	private int memCounter;
+	// XXX: salvar ultima label de saída para break???
+
+	public IntermediateCodeGenerator(SemanticAnalyzer semanticAnalyzer) {
+		this.tokens = semanticAnalyzer.getTokens();
+		this.intermediateCode = new StringBuilder();
+		this.seenIds = new HashSet<Integer>();
+		this.labelCounter = 0;
+		this.memCounter = 0;
+	}
+
+	public void run() {
+		for (int i = 0; i < this.tokens.size(); i++) {
+			if (!this.seenIds.contains(i)) {
+				this.seenIds.clear();
+				this.operate(i);
 			}
-			
-			intermediateCode.append("FINAL:");
-			intermediateCode.append(BREAK_LINE);
-			intermediateCode.append("exit");
 		}
 
-		private void operate(int index) {
-			this.seenIds.add(index);
-			Token token = tokens.get(index);
-			if (TokenType.PR.equals(token.getType())) {
-				if (Constants.BREAK.equals(token.getName())) {
-					createToBreak(index);
-				} else if (Constants.FOR.equals(token.getName())) {
-					createToFor(index);
-				} else if (Constants.IF.equals(token.getName())) {
-					createToIf(index);
-				} else if (Constants.IFELSE.equals(token.getName())) {
-					createToIfElse(index);
-				} else if (Constants.RETURN.equals(token.getName())) {
-					createToReturn(index);
-				} else if (Constants.PRINT.equals(token.getName())) {
-					createToReadOrPrint(index, "out");
-				} else if (Constants.READ.equals(token.getName())) {
-					createToReadOrPrint(index, "in");
-				}
-			} else if (TokenType.IDENT.equals(token.getType())) { 
-				createToIdent(index);
+		this.intermediateCode.append("FINAL:");
+		this.intermediateCode.append(BREAK_LINE);
+		this.intermediateCode.append("exit");
+	}
+
+	private void operate(int index) {
+		this.seenIds.add(index);
+		Token token = this.tokens.get(index);
+		if (TokenType.PR.equals(token.getType())) {
+			if (Constants.BREAK.equals(token.getName())) {
+				this.createToBreak(index);
+			} else if (Constants.FOR.equals(token.getName())) {
+				this.createToFor(index);
+			} else if (Constants.IF.equals(token.getName())) {
+				this.createToIf(index);
+			} else if (Constants.IFELSE.equals(token.getName())) {
+				this.createToIfElse(index);
+			} else if (Constants.RETURN.equals(token.getName())) {
+				this.createToReturn(index);
+			} else if (Constants.PRINT.equals(token.getName())) {
+				this.createToReadOrPrint(index, "out");
+			} else if (Constants.READ.equals(token.getName())) {
+				this.createToReadOrPrint(index, "in");
+			}
+		} else if (TokenType.IDENT.equals(token.getType())) {
+			this.createToIdent(index);
+		} else {
+			// XXX: verificar se há problema com a linha
+			this.intermediateCode.append(token.getName());
+		}
+	}
+
+	private void createToReadOrPrint(int index, String toAppend) {
+		this.intermediateCode.append(toAppend);
+		for (int i = index + 1; i < this.tokens.size(); i++) {
+			this.seenIds.add(i);
+			Token token = this.tokens.get(i);
+			if (this.isSemiColon(token)) {
+				break;
 			} else {
-				// XXX: verificar se há problema com a linha
-				intermediateCode.append(token.getName());
+				this.intermediateCode.append(" " + token.getName());
 			}
 		}
-		
-		private void createToReadOrPrint(int index, String toAppend) {
-			intermediateCode.append(toAppend);
-			for (int i = index + 1; i < tokens.size(); i++) {
-				this.seenIds.add(i);
-				Token token = tokens.get(i);
-				if (TokenType.DELIMITER.equals(token.getType()) && ";".equals(token.getName())) {
-					break;
-				} else {
-					intermediateCode.append(" " + token.getName());
-				}
+		this.intermediateCode.append(BREAK_LINE);
+	}
+
+	private void createToIdent(int index) {
+		this.appendIdent(index);
+
+		for (int i = index + 1; i < this.tokens.size(); i++) {
+			this.seenIds.add(i);
+			Token token = this.tokens.get(i);
+			if (this.isSemiColon(token)) {
+				break;
+			} else if (TokenType.IDENT.equals(token.getType())) {
+				this.appendIdent(i);
 			}
-			intermediateCode.append(BREAK_LINE);
 		}
+	}
 
-		private void createToIdent(int index) {
-			
-		}
+	private void appendIdent(int index) {
+		String toAppend = "$" + this.memCounter + " = " + this.tokens.get(index).getName();
+		this.intermediateCode.append(toAppend);
+		this.intermediateCode.append(BREAK_LINE);
+		this.memCounter++;
+	}
 
-		private void createToReturn(int index) {
-			intermediateCode.append("GOTO FINAL");
-			for (int i = index + 1; i < tokens.size(); i++) {
-				this.seenIds.add(i);
-				Token token = tokens.get(i);
-				if (TokenType.DELIMITER.equals(token.getType()) && ";".equals(token.getName())) {
-					break;
-				}
+	private void createToReturn(int index) {
+		this.intermediateCode.append("GOTO FINAL");
+		for (int i = index + 1; i < this.tokens.size(); i++) {
+			this.seenIds.add(i);
+			Token token = this.tokens.get(i);
+			if (this.isSemiColon(token)) {
+				break;
 			}
-			intermediateCode.append(BREAK_LINE);
-			
 		}
+		this.intermediateCode.append(BREAK_LINE);
 
-		private void createToIfElse(int index) {
-			// TODO Auto-generated method stub
-			
-		}
+	}
 
-		private void createToIf(int index) {
-			// TODO Auto-generated method stub
-			
-		}
+	private void createToIfElse(int index) {
+		// TODO Auto-generated method stub
 
-		private void createToFor(int index) {
-			// TODO Auto-generated method stub
-			
-		}
+	}
 
-		private void createToBreak(int index) {
-			// TODO Auto-generated method stub
-			
-		}
+	private void createToIf(int index) {
+		// TODO Auto-generated method stub
 
-		public void print() {
-			System.out.println(intermediateCode.toString());
-		}
+	}
+
+	private void createToFor(int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void createToBreak(int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean isSemiColon(Token token) {
+		return TokenType.DELIMITER.equals(token.getType()) && ";".equals(token.getName());
+	}
+
+	public void print() {
+		System.out.println(this.intermediateCode.toString());
+	}
 
 }
